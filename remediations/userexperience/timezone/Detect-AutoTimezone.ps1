e2382ef4486389f4880cef364ceb7579e852c5c5
yarivanos <yari.vanos@it2grow.nl>
2025-12-13 16:45:30 +0100

    Update Detect-AutoTimezone.ps1

--- remediations/userexperience/timezone/Detect-AutoTimezone.ps1
+++ remediations/userexperience/timezone/Detect-AutoTimezone.ps1
@@ -1,1 +1,10 @@
+# Detection script for automatic time zone
+$tzAutoUpdate = Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\tzautoupdate" -Name "Start" -ErrorAction SilentlyContinue
 
+if ($tzAutoUpdate.Start -eq 3) {
+    Write-Output "Automatic time zone is enabled"
+    exit 0
+} else {
+    Write-Output "Automatic time zone is not enabled"
+    exit 1
+}
