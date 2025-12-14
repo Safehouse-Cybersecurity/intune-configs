896f35e59ad44eef57f64d1bd6f6b3d8021df51a
yarivanos <yari.vanos@it2grow.nl>
2025-12-14 15:04:48 +0100

    Update Remediate-Taskbar.ps1

--- remediations/userexperience/taskbar/Remediate-Taskbar.ps1
+++ remediations/userexperience/taskbar/Remediate-Taskbar.ps1
@@ -1,5 +1,4 @@
 # Remediation script for taskbar settings
-# Sets TaskbarGlomLevel and MMTaskbarMode
 
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 
@@ -8,7 +7,10 @@ try {
     Set-ItemProperty -Path $regPath -Name "TaskbarGlomLevel" -Value 1 -Type DWord -Force
     
     # MMTaskbarMode: 0 = All taskbars, 1 = Main and where open, 2 = Where open only
-    Set-ItemProperty -Path $regPath -Name "MMTaskbarMode" -Value 0 -Type DWord -Force
+    Set-ItemProperty -Path $regPath -Name "MMTaskbarMode" -Value 2 -Type DWord -Force
+    
+    # MMTaskbarGlomLevel: 0 = Always combine, 1 = Combine when full, 2 = Never combine
+    Set-ItemProperty -Path $regPath -Name "MMTaskbarGlomLevel" -Value 1 -Type DWord -Force
     
     # Restart Explorer to apply changes
     Stop-Process -Name "explorer" -Force -ErrorAction SilentlyContinue
