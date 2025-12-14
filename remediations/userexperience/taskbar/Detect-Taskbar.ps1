1c4e3292f2f454ae766270afb07f11805467ff8f
yarivanos <yari.vanos@it2grow.nl>
2025-12-14 15:04:04 +0100

    Update Detect-Taskbar.ps1

--- remediations/userexperience/taskbar/Detect-Taskbar.ps1
+++ remediations/userexperience/taskbar/Detect-Taskbar.ps1
@@ -1,16 +1,17 @@
 # Detection script for taskbar settings
-# Checks TaskbarGlomLevel and MMTaskbarMode
 
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 
 $taskbarGlom = Get-ItemProperty -Path $regPath -Name "TaskbarGlomLevel" -ErrorAction SilentlyContinue
 $mmTaskbar = Get-ItemProperty -Path $regPath -Name "MMTaskbarMode" -ErrorAction SilentlyContinue
+$mmTaskbarGlom = Get-ItemProperty -Path $regPath -Name "MMTaskbarGlomLevel" -ErrorAction SilentlyContinue
 
 # Expected values:
 # TaskbarGlomLevel = 1 (Combine when taskbar is full)
-# MMTaskbarMode = 0 (Show on all taskbars) - change if you want different
+# MMTaskbarMode = 2 (Taskbar where window is open)
+# MMTaskbarGlomLevel = 1 (Combine when taskbar is full on other taskbars)
 
-if ($taskbarGlom.TaskbarGlomLevel -eq 1 -and $mmTaskbar.MMTaskbarMode -eq 0) {
+if ($taskbarGlom.TaskbarGlomLevel -eq 1 -and $mmTaskbar.MMTaskbarMode -eq 2 -and $mmTaskbarGlom.MMTaskbarGlomLevel -eq 1) {
     Write-Output "Taskbar settings are correct"
     exit 0
 } else {
