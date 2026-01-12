f0c5d78498f20b60bc6a842a892a2bde0d594def
yarivanos <yari.vanos@it2grow.nl>
2026-01-12 10:00:41 +0100

    Update Detect-Lockscreen.ps1

--- remediations/userexperience/lockscreen/Detect-Lockscreen.ps1
+++ remediations/userexperience/lockscreen/Detect-Lockscreen.ps1
@@ -2,7 +2,7 @@
 
 try {
     $LockscreenPath = "C:\ProgramData\it2grow\lockscreen.png"
-    $ExpectedHash = "21A26525B95C47DCE3D75FB32964D36DC97FCF5B53AF61E92D674457B862B767"
+    $ExpectedHash = "A8AF5BD0A9358108A2535445BFA0E20E69AA5BB8F4039EFBD5110FC72A640B84"
     
     if (!(Test-Path $LockscreenPath)) {
         Write-Output "File missing"
